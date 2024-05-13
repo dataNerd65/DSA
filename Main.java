3a986643dfcc0e90512455fa94bdb3e414bf21b9
dataNerd65 <pkiarie811@gmail.com>
2024-05-13 05:46:02 +0300

    Java version of sortEx

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void sortInExample(int[] myArray){
+        int minVal = myArray[0];
+
+        for (int i : myArray){
+            if (i < minVal){
+                minVal = i;
+            }
+        }
+        System.out.println("Lowest value: " + minVal);
+    }
+    public static void main(String[] args){
+        sortInExample(new int[] {7, 12, 9, 4, 11});
+        sortInExample(new int[] {3, 5, 2, 1});
+    }
+
+}
