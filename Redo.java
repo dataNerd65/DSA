fb1f88672fdd51cdf6fefd48a17ade558fc97df6
dataNerd65 <pkiarie811@gmail.com>
2024-05-13 18:08:09 +0300

    Bubble sort implemented

--- Redo.java
+++ Redo.java
@@ -9,16 +9,18 @@ public class Redo {
 
         }
         System.out.println("FirstNo." + miniVal);
-
+//Logic of printing the arrays...
         int n = array.length;
-        for (int i = 0; i < n-1; i++){
-            for (int j = 0;  j < n-i-1; j++){
+        for (int k = 0; k < n-1; k++){
+            for (int j = 0;  j < n-k-1; j++){
+                if (array[j] > array[j+1]){ // Was missing this statement
                 //Swapping array[j] and array[j+1]
                 int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
         }
+    }
         //printing the sorted array
         System.out.print("Sorted Array: ");//Using print statement to print the numbers of array in oneline
         for(int num: array){
@@ -27,6 +29,7 @@ public class Redo {
         System.out.println();
         
     }
+    //...In one line and sorted
 
     public static void main(String[] args){
         SortArray(new int[] {7, 12, 9, 4, 11});
