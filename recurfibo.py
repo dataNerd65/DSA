6697cd9340a82f233f8fb29df1c23c088021b292
dataNerd65 <pkiarie811@gmail.com>
2024-05-14 06:28:13 +0300

    Using recursion to implement fibonacci generation

---
+++ recurfibo.py
@@ -0,0 +1,26 @@
+class RecurFibo:
+    def __init__(self):
+
+        self.count = 2
+
+    def fibonaci(self, prev1, prev2): #when calling this method recursively, use self.fibonacci
+        global count #Calling the globally initialized count variable
+
+        if self.count <= 19:
+            newFibo = prev1 + prev2
+            print(newFibo)
+            prev2 = prev1
+            prev1 = newFibo
+            self.count += 1
+
+            self.fibonaci(prev1, prev2)
+        else:
+            return
+        
+fibo = RecurFibo()
+print(0)
+print(1)
+fibo.fibonaci(1, 0)
+
+
+
