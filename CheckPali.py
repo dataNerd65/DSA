c82ce98a45ef27a84023dcac7960816e3a8b307a
dataNerd65 <pkiarie811@gmail.com>
2024-05-29 20:41:11 +0300

    mods to check well if an integer is a palindrome

--- CheckPali.py
+++ CheckPali.py
@@ -3,6 +3,8 @@ class Solution(object):
         #If number is -ve or (if its not o but ends with zero) not a palindrome
         if x < 0 or (x !=0 and x % 10 == 0):
             return False
+        #Reversing Half: We reverse the last half of the number and compare it with the first half. 
+        #If they are equal, then the number is a palindrome.
         
         reversed_half = 0
         while x > reversed_half:
