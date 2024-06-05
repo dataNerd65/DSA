9dcc4b3f60ad555a66a6a38c4aba1e8c010c0bcd
dataNerd65 <pkiarie811@gmail.com>
2024-06-05 06:04:28 +0300

    Documented the soln

--- Majority.py
+++ Majority.py
@@ -1,17 +1,28 @@
 class Solution:
+    #The purpose of the following method is to find the majority element in an array
     def MajorityElement(self, nums):
+        #candidate variable is used to store current candidate for the majority element
         candidate = None
+
+        #count variable counts occurrences of the candidate
         count = 0
         
+        #Iterating over the list
         for num in nums:
+            #if count == 0, it sets current no. as candidate and sets count to 1
             if count == 0:
                 candidate = num
                 count = 1
+            
+            #If current no. is the same as candidate , it increments count
 
             elif num == candidate:
                 count += 1
+                #if differnt , decrement count
             else:
                 count -= 1
+            
+        #After iteration over all elements, candidate will be the majority element
 
         return candidate
     
@@ -22,3 +33,4 @@ print(soln.MajorityElement(nums))
 
 nums2 =[3, 2, 3]
 print(soln.MajorityElement(nums2))
+
