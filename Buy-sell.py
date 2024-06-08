1e873070f144efd8b2734848b217fd407f22b061
dataNerd65 <pkiarie811@gmail.com>
2024-06-08 09:42:55 +0300

    Implememted successfully

--- Buy-sell.py
+++ Buy-sell.py
@@ -3,17 +3,27 @@ class Solution:
         if not prices:
             return 0
         
+        #To store maximum profit calculated so far
         min_price =float('inf')
-        max_profit = 0
+        #min_price keeps track of the minimum price encountered so far while iterating through the prices
+        max_profit = 0 
+        #To store maximum profit calculated so far
+      
+        #iterating through the list
 
+        #For each price update 'min_price'
         for price in prices:
+            #if current price is lower than min_price
             if price < min_price:
                 min_price = price
             potential_profit = price - min_price
 
             if potential_profit > max_profit:
                 max_profit = potential_profit
+        
 
+        #After iterating through the list, 'max_profit' will contain the maximum profit possible
+        #if no profit made(prices are in descending order) 'max_profit' will remain 0
         return max_profit
     
 soln = Solution()
@@ -22,3 +32,4 @@ prices2 = [7, 6, 4, 3, 1]
 
 print(soln.maxProfit(prices))
 print(soln.maxProfit(prices2))
+
---
+++ maxJump.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxJump(self, nums):
+        
+        maxReachable = 0
+
+        for i in range(len(nums)):
+            if i > maxReachable:
+                return False
+            
+            maxReachable = max(maxReachable, i + nums[i])
+
+            if maxReachable >= len(nums) -1:
+                return True
+            
+        return maxReachable >= len(nums) -1
+    
+soln = Solution()
+nums = [2, 3, 1, 1, 4]
+print(soln.maxJump(nums))
+
+nums2 = [3, 2, 1, 0, 4]
+print(soln.maxJump(nums2))
