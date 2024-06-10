4d79fb0a1df72cbe48d9e23502491666a3879077
dataNerd65 <pkiarie811@gmail.com>
2024-06-10 06:44:52 +0300

    Demonsstrated the use of rsrtip() that is used to remove trailing spaces and split() that is used to split a string into a list where each word is a separate element of the list

--- LenLastWord.py
+++ LenLastWord.py
@@ -12,12 +12,32 @@ class Solution:
         return len(lastWord)
     
 soln = Solution()
-s = "Hello World"
+s = "Hello World "
 s1 = "   fly me   to   the moon  "
 s2 = "luffy is still joyboy"
 
 print(soln.lenOfLastWord(s))
 print(soln.lenOfLastWord(s1))
 print(soln.lenOfLastWord(s2))
+
+v = "Hello World "
+x = "   fly me   to   the moon  "
+b = "luffy is still joyboy"
+c = v.rstrip()
+d = x.rstrip()
+e = b.rstrip()
+print(c)
+print(d)
+print(e)
+
+m = c.split()
+n = d.split()
+o = e.split()
+print(m)
+print(n)
+print(o)
+print('r=' , m[-1], 'f=', n[-1], 'h=', o[-1])
+
+
 
 
